433c8c8b7b075e2b20b9e8c13b6920d9bad4a76c
Jaehui Yu <pkj0522@naver.com>
2025-02-12 17:40:39 +0900

    Initial Commit

---
+++ DoodleZip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoodleZip
+//
+//  Created by Jaehui Yu on 2/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoodleZip/DoodleZipApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoodleZipApp.swift
+//  DoodleZip
+//
+//  Created by Jaehui Yu on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DoodleZipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
